57eac6c3817ebf92e4d3878479cac81b941868bb
astha niharika <astha.niharika@hyscaler.com>
2024-08-01 00:15:10 +0530

    added

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -7,6 +7,8 @@ const axiosInstance = axios.create({
   },
 });
 
+axiosInstance.defaults.withCredentials = true;
+
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
